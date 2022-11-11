bc0b4cb8cd3759b28dd22abc853e8b24bd6e795d
Tseng <a0979085925@gmail.com>
2022-11-11 16:07:40 +0800

    no message

--- ex2.php
+++ ex2.php
@@ -14,6 +14,7 @@ echo "Git測試4-1";
 echo "devdevdevdevdev";
 echo "bugfixbugfix";
 echo "bugfixbug%%";
+echo "bugfixbug88";
 for ($i = 0; $i < count($array); $i++) {
     $total1 = $total1 + $array[$i][0];
     $total2 = $total2 + $array[$i][1];
