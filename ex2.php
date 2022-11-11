a2c36edeb43209e95989dde0c2432c527f0748fa
Tseng <a0979085925@gmail.com>
2022-11-11 11:03:59 +0800

    init

---
+++ ex2.php
@@ -0,0 +1,16 @@
+<?php
+$array = [[101, 200], [102, 201, '', '', 5], [104, 203], [105, 205], [106, 300]];
+$array1[2][5] = [1, 2, 5];
+echo $array1[2][5];
+$total1 = 0;
+$total2 = 0;
+$total3 = 0;
+
+for ($i = 0; $i < count($array); $i++) {
+    $total1 = $total1 + $array[$i][0];
+    $total2 = $total2 + $array[$i][1];
+    for ($j = 0; $j < 2; $j++) {
+        $total3 = $total3 + $array[$i][$j];
+    }
+}
+echo '小計' . $total1 . '<br>小計' . $total2 . '<br>總計' . $total3;
